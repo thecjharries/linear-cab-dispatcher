d65a19f8e240a5b37dc20c06454eeed1836b3fe0
CJ Harries <cj@wizardsoftheweb.pro>
2023-03-11 20:01:52 -0600

    Create drainComments

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import {Issue, LinearClient, Team} from '@linear/sdk';
+import {Comment, Issue, LinearClient, Team} from '@linear/sdk';
 import {env} from 'process';
 
 // Api key authentication
@@ -18,13 +18,28 @@ function drainIssues(team: Team, start?: string): Promise<Issue[]> {
   });
 }
 
+function drainComments(issue: Issue, start?: string): Promise<Comment[]> {
+  const args = start ? {after: start, first: 50} : undefined;
+  return issue.comments(args).then(({nodes, pageInfo}) => {
+    if (pageInfo.hasNextPage) {
+      return drainComments(issue, pageInfo.endCursor).then(nextNodes => {
+        return nodes.concat(nextNodes);
+      });
+    }
+    return nodes;
+  });
+}
+
 linearClient
   .team(env.TEAM_ID as string)
   .then(team => {
     return drainIssues(team);
   })
   .then(issues => {
-    console.log(issues.length);
+    return drainComments(issues[0]);
+  })
+  .then(comments => {
+    console.log(comments);
   })
   .catch(err => {
     console.log(err);
